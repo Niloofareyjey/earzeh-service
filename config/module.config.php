1d00a105d8ed0fd05ec0bc766de548e041e6dac2
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2015-10-06 18:59:42 +0330

    update 1

---
+++ config/module.config.php
@@ -0,0 +1,81 @@
+<?php
+namespace User;
+
+return array(
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__.'_driver' => array(
+                'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/'.__NAMESPACE__.'/Entity')
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+
+            )),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+        'template_map' => array(
+
+        )
+    ),
+
+    'controllers' => array(
+        'factories' => array(
+        // for passing variables
+        ),
+        'invokables' =>array(
+            //without passing variable controlers
+        )
+    ),
+
+    'service_manager' => array(
+        'factories' => array(
+            'Ellie\Service\Log' => 'Ellie\Service\Log\LogServiceFactory',
+           // 'Ellie\Service\Authentication' => 'Ellie\Service\Authentication\ServiceFactory',
+        )
+    ),
+
+    // This lines opens the configuration for the RouteManager
+    'router' => array(
+        // Open configuration for all possible routes
+        'routes' => array(
+            // Define a new route called "post"
+            'user' => array(
+                // Define the routes type to be "Zend\Mvc\Router\Http\Literal", which is basically just a string
+                'type' => 'segment',
+                // Configure the route itself
+                'options' => array(
+                    // Listen to "/blog" as uri
+                    'route'    => '/services[/:controller[/:action]]',
+                    'constraints' => array(
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+
+                    ),
+                    // Define default controller and action to be called when this route is matched
+                    'defaults' => array(
+                        'controller' => 'index',
+                        'action'     => 'index',
+                    )
+                )
+            )
+        )
+    ),
+
+//    'menu'  => [
+//        'User Management' => [
+//            'Create New User' => 'manage/create',
+//            'Company List' => 'manage/list',
+//            'Manager List' => 'user/manage/',
+//            'Operator List' => 'user/manage/',
+//            'Unregisted List' => 'user/manage/',
+//        ]
+//    ]
+);
---
+++ src/Service/Controller/Management.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: pooria
+ * Date: 10/6/15
+ * Time: 3:19 PM
+ */
+namespace Service\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;// for run in zend's MVC
+use Ellie\Interfaces\ControllerInterface;
+use Zend\View\Model\ViewModel;
+
+class Management extends  AbstractActionController
+    implements ControllerInterface
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function createAction()
+    {
+
+    }
+
+    public function editAction()
+    {
+        // TODO: Implement editAction() method.
+    }
+
+    public function deleteAction()
+    {
+        // TODO: Implement deleteAction() method.
+    }
+
+    public function listAction()
+    {
+        // TODO: Implement listAction() method.
+    }
+}
